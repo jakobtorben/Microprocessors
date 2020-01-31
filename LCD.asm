a961c46e844d7b32ac99d795b32db61d3fa52da5
jrt3817 <jrt3817@skbl-422-015.ph.ic.ac.uk>
2020-01-31 09:36:07 +0000

    keypad

--- LCD.asm
+++ LCD.asm
@@ -1,13 +1,14 @@
 #include p18f87k22.inc
 
-    global  LCD_Setup, LCD_Write_Message
+    global  LCD_Setup, LCD_Write_Message, LCD_Send_Byte_I, LCD_delay_x4us, LCD_Write_Message2
+    extern  myTable;, myTable_l
 
 acs0    udata_acs   ; named variables in access ram
 LCD_cnt_l   res 1   ; reserve 1 byte for variable LCD_cnt_l
 LCD_cnt_h   res 1   ; reserve 1 byte for variable LCD_cnt_h
 LCD_cnt_ms  res 1   ; reserve 1 byte for ms counter
 LCD_tmp	    res 1   ; reserve 1 byte for temporary use
-LCD_counter res 1   ; reserve 1 byte for counting through nessage
+LCD_counter res 1   ; reserve 1 byte for counting through message
 
 	constant    LCD_E=5	; LCD enable bit
     	constant    LCD_RS=4	; LCD register select bit
@@ -20,26 +21,32 @@ LCD_Setup
 	movwf	TRISB
 	movlw   .40
 	call	LCD_delay_ms	; wait 40ms for LCD to start up properly
+	
 	movlw	b'00110000'	; Function set 4-bit
 	call	LCD_Send_Byte_I
 	movlw	.10		; wait 40us
 	call	LCD_delay_x4us
+	
 	movlw	b'00101000'	; 2 line display 5x8 dot characters
 	call	LCD_Send_Byte_I
 	movlw	.10		; wait 40us
 	call	LCD_delay_x4us
+	
 	movlw	b'00101000'	; repeat, 2 line display 5x8 dot characters
 	call	LCD_Send_Byte_I
 	movlw	.10		; wait 40us
 	call	LCD_delay_x4us
+	
 	movlw	b'00001111'	; display on, cursor on, blinking on
 	call	LCD_Send_Byte_I
 	movlw	.10		; wait 40us
 	call	LCD_delay_x4us
+	
 	movlw	b'00000001'	; display clear
 	call	LCD_Send_Byte_I
 	movlw	.2		; wait 2ms
 	call	LCD_delay_ms
+	
 	movlw	b'00000110'	; entry mode incr by 1 no shift
 	call	LCD_Send_Byte_I
 	movlw	.10		; wait 40us
@@ -55,6 +62,26 @@ LCD_Loop_message
 	bra	LCD_Loop_message
 	return
 
+LCD_Write_Message2	    ; Message stored at FSR2, length stored in W
+	movwf   LCD_counter
+
+	movlw	upper(myTable)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(myTable)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(myTable)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	;movlw	myTable_l	; bytes to read
+	;movwf 	counter		; our counter register 	
+	
+LCD_Loop_message2
+	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+	movf	TABLAT, W; move data from TABLAT to (FSR0), inc FSR0	
+	call    LCD_Send_Byte_D
+	decfsz  LCD_counter
+	bra	LCD_Loop_message2
+	return
+	
 LCD_Send_Byte_I		    ; Transmits byte stored in W to instruction reg
 	movwf   LCD_tmp
 	swapf   LCD_tmp,W   ; swap nibbles, high nibble goes first
