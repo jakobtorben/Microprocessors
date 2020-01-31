906b0f9638c5d96f1365a6f162062ab0820a9849
jrt3817 <jrt3817@skbl-422-015.ph.ic.ac.uk>
2020-01-31 09:31:44 +0000

    keypad

--- Simple1.asm
+++ Simple1.asm
@@ -1,11 +1,16 @@
 	#include p18f87k22.inc
 
 	extern	UART_Setup, UART_Transmit_Message  ; external UART subroutines
-	extern  LCD_Setup, LCD_Write_Message	    ; external LCD subroutines
+	extern  LCD_Setup, LCD_Write_Message, LCD_Send_Byte_I, LCD_delay_x4us, LCD_Write_Message2	    ; external LCD subroutines
+	global	myTable;, myTable_l
 	
 acs0	udata_acs   ; reserve data space in access ram
 counter	    res 1   ; reserve one byte for a counter variable
 delay_count res 1   ; reserve one byte for counter in the delay routine
+keyread1    res 1   ; reserve one byte for the keypad value maybe
+rowval	    res 1   ; reserves one byte for row value
+colval	    res 1   ; reserves one byte for column value
+button      res 1   ; reserves one byte for key value
 
 tables	udata	0x400    ; reserve data anywhere in RAM (here at 0x400)
 myArray res 0x80    ; reserve 128 bytes for message data
@@ -15,7 +20,7 @@ rst	code	0    ; reset vector
 
 pdata	code    ; a section of programme memory for storing data
 	; ******* myTable, data in programme memory, and its length *****
-myTable data	    "Hello World!\n"	; message, plus carriage return
+myTable data	    "Hello world!\n"	; message, plus carriage return
 	constant    myTable_l=.13	; length of data
 	
 main	code
@@ -24,36 +29,212 @@ setup	bcf	EECON1, CFGS	; point to Flash program memory
 	bsf	EECON1, EEPGD 	; access Flash program memory
 	call	UART_Setup	; setup UART
 	call	LCD_Setup	; setup LCD
+	clrf	LATE
+	setf	TRISE ; Tri-state PortE
+	banksel PADCFG1 ; PADCFG1 is not in Access Bank!!
+	bsf	PADCFG1, REPU, BANKED ; PortE pull-ups on
+	movlw	0x00
+	movwf	TRISD
+	
 	goto	start
 	
 	; ******* Main programme ****************************************
-start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
-	movlw	upper(myTable)	; address of data in PM
-	movwf	TBLPTRU		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter		; our counter register
-loop 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter		; count down to zero
-	bra	loop		; keep going until finished
-		
-	movlw	myTable_l-1	; output message to LCD (leave out "\n")
-	lfsr	FSR2, myArray
-	call	LCD_Write_Message
+start 	
+	movlw	0x0f
+	movwf	TRISE
+	movff	PORTE, keyread1
 	
-	movlw	myTable_l	; output message to UART
-	lfsr	FSR2, myArray
-	call	UART_Transmit_Message
+;	cpfseq	keyread1, b'00000000'
+	
+	
+row1	
+	movlw	b'00000111'
+	cpfseq	keyread1
+	bra     row2
+	movwf	rowval
+	bra     colcheck
+	
+row2	movlw	b'00001011'
+	cpfseq	keyread1
+	bra     row3
+	movwf	rowval
+	bra     colcheck
+	
+row3	movlw	b'00001101'
+	cpfseq	keyread1
+	bra     row4
+	movwf	rowval
+	bra     colcheck
+	
+row4	movlw	b'00001110'
+	cpfseq	keyread1
+	bra     start
+	movwf	rowval
+	bra     colcheck
+	
+	
+colcheck
+	movlw	0xf0
+	movwf	TRISE
+	movff	PORTE, keyread1
+	
+col1
+	movlw	b'01110000'
+	cpfseq	keyread1
+	bra     col2
+	movwf	colval
+	bra     determine
+	
+col2	movlw	b'10110000'
+	cpfseq	keyread1
+	bra     col3
+	movwf	colval
+	bra     determine
+	
+col3	movlw	b'11010000'
+	cpfseq	keyread1
+	bra     col4
+	movwf	colval
+	bra     determine
+	
+col4	movlw	b'11100000'
+	cpfseq	keyread1
+	bra     start
+	movwf	colval
+	bra     determine
+	
+determine
+	movff	rowval, W
+	addwf	colval
+	bra	check0
+	
+check0	movlw   0xBE
+	cpfseq  colval
+	bra     check1
+	movlw   0x0
+	movwf   button
+	bra     output
+	
+check1	movlw   0x77
+	cpfseq  colval
+	bra     check2
+	movlw   0x1
+	movwf   button
+	bra     output
+	
+check2	movlw   0xB7
+	cpfseq  colval
+	bra     check3
+	movlw   0x2
+	movwf   button
+	bra     output
+	
+check3	movlw   0xD7
+	cpfseq  colval
+	bra     check4
+	movlw   0x3
+	movwf   button
+	bra     output
+	
+check4	movlw   0x7B
+	cpfseq  colval
+	bra     check5
+	movlw   0x4
+	movwf   button
+	bra     output
+	
+check5	movlw   0xBB
+	cpfseq  colval
+	bra     check6
+	movlw   0x5
+	movwf   button
+	bra     output
+	
+check6	movlw   0xDB
+	cpfseq  colval
+	bra     check7
+	movlw   0x6
+	movwf   button
+	bra     output
+	
+check7	movlw   0x7D
+	cpfseq  colval
+	bra     check8
+	movlw   0x7
+	movwf   button
+	bra     output
+	
+check8	movlw   0xBD
+	cpfseq  colval
+	bra     check9
+	movlw   0x8
+	movwf   button
+	bra     output
+	
+check9	movlw   0xDD
+	cpfseq  colval
+	bra     checkA
+	movlw   0x9
+	movwf   button
+	bra     output
+	
+checkA	movlw   0x7E
+	cpfseq  colval
+	bra     checkB
+	movlw   0xA
+	movwf   button
+	bra     output
+	
+checkB	movlw   0xDE
+	cpfseq  colval
+	bra     checkB
+	movlw   0xB
+	movwf   button
+	bra     output
+	
+checkC	movlw   0xEE
+	cpfseq  colval
+	bra     checkD
+	movlw   0xC
+	movwf   button
+	bra     output
+	
+checkD	movlw   0xED
+	cpfseq  colval
+	bra     checkE
+	movlw   0xD
+	movwf   button
+	bra     output
+	
+checkE	movlw   0xEB
+	cpfseq  colval
+	bra     checkF
+	movlw   0xE
+	movwf   button
+	bra     output
+	
+checkF	movlw   0xE7
+	cpfseq  colval
+	bra     errmsg
+	movlw   0xF
+	movwf   button
+	bra     output
 
-	goto	$		; goto current line in code
+	
+output
+	bra	LCD_Send_Byte_I
+errmsg	
+	
+	
+	movff	PORTE, PORTD	
+	goto	start
 
 	; a delay subroutine if you need one, times around loop in delay_count
 delay	decfsz	delay_count	; decrement until zero
 	bra delay
 	return
+	
+	
+
 
 	end
